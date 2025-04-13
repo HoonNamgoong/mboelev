eb0ac4bb4564798e65db209237101f51332ef391
mboelev <mboelev@daum.net>
2025-04-14 01:06:50 +0900

    Initial commit

---
+++ class/pages/section01/01-01-example/index.js
@@ -0,0 +1,12 @@
+export default function 나만의페이지() {
+    /*자바스크립트 구역 */
+    const aaa = 3 
+    return (
+        //html작성 구역
+        <div>
+            <div>철수</div>
+            <button>클릭하세요!!</button>
+        </div>
+    )
+    
+}
