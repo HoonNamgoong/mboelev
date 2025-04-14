5681bff288e2dd49d620c7aa7d0a0f186d676f96
lkrentcar <lk200701@naver.com>
2025-04-14 11:15:14 +0900

    css실험해봄

--- class/pages/index.js
+++ class/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -9,8 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div>잘 나오나용용</div>
+      <main className={styles.main}>
+        <code className={styles.code}>잘 나오나용용</code>
+      </main>
     </>
   );
 }
